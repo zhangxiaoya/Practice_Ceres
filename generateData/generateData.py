81528e899e2bf553338556ccbfdbab5ec35fc23f
zhangxiaoya <zhangxiaoya1989@gmail.com>
2018-08-15 15:55:15 +0800

    plot

--- generateData/generateData.py
+++ generateData/generateData.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as mpl
 
 m = 0.3
 c = 0.1
@@ -8,7 +9,7 @@ y = np.exp(x * m + c)
 
 np.random.seed(123456)
 
-noise = np.random.randn(np.size(x)) + 0.2
+noise = np.random.randn(np.size(x)) * 0.2
 
 yObserved = y + noise
 print(x.shape)
@@ -17,3 +18,13 @@ print(yObserved.shape)
 
 for [ye, yoe] in zip(y, yObserved):
     print("{} to {}".format(ye, yoe))
+
+mpl.figure()
+mpl.plot(x, y, 'g*-')
+mpl.plot(x, yObserved, 'r--')
+mpl.xlabel("x")
+mpl.ylabel("y")
+mpl.show()
+
+
+# plot reference: https://www.cnblogs.com/nxld/p/7435930.html
